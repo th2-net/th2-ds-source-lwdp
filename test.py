94584f7522a8f3d8d6499bb0ba1543d2c3aaeca6
Luka Turabelidze <luka.turabelidze@de-vws144.exp.exactpro.com>
2022-09-13 14:25:37 +0000

    removed http classes

---
+++ test.py
@@ -0,0 +1,4 @@
+from th2_data_services_lwdp.events_tree import EventsTreeCollectionProvider6 as ETC
+from th2_data_services_lwdp.data_source import GRPCProvider6DataSource
+
+data_source = GRPCProvider6DataSource('http://10.100.66.114:31787')
