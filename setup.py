60282d201063fc67ce8eeb3b1ea52e72af2033bf
turikotura <turikotura@outlook.com>
2022-09-19 16:10:54 +0400

    fixed version info and setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     license="Apache License 2.0",
     python_requires=">=3.7",
     install_requires=requirements,
-    packages=find_packages(include=["th2_data_services", "th2_data_services.*"]),
+    packages=find_packages(include=["th2_data_services_lwdp", "th2_data_services_lwdp.*"]),
     include_package_data=True,
 )
