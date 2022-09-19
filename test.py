5c79980dc6167e4ff836e78e2da40837ed320b1d
turikotura <turikotura@outlook.com>
2022-09-19 17:00:54 +0400

    publish commit

--- test.py
+++ test.py
@@ -1,4 +1,2 @@
 from th2_data_services_lwdp.events_tree.events_tree_collection import EventsTreeCollectionProvider as ETC
 from th2_data_services_lwdp.provider_api.grpc import GRPCAPI
-from th2_data_services_lwdp.data_source import GRPCDataSource
-
