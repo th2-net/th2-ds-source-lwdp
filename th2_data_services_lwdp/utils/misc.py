22ca1e5a2ab6cbeaa36532b4118fd25cdddd687a
Slava Ermakov <connect-soft@yandex.ru>
2022-12-20 11:23:48 +0300

    Update misc.py

--- th2_data_services_lwdp/utils/misc.py
+++ th2_data_services_lwdp/utils/misc.py
@@ -6,11 +6,13 @@ def check_list_or_tuple(variable, var_name):  # noqa
         raise TypeError(f"{var_name} argument has to be list or tuple type. Got {type(variable)}")
 
 
+# TODO - move to DS core
 def datetime2ms(dt_timestamp: datetime):
     """Epoch time in milliseconds."""
     return int(1000 * dt_timestamp.replace(tzinfo=timezone.utc).timestamp())
 
 
+# TODO - move to DS core
 def seconds2ms(timestamp: int):
     """Epoch time in milliseconds."""
     return int(1000 * timestamp)
