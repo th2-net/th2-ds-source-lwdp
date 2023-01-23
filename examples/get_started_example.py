afd8de64bf0d9ba4026bd72dafe784efaed29595
turikotura <turikotura@outlook.com>
2023-01-23 13:40:39 +0000

    Added info about streams

--- examples/get_started_example.py
+++ examples/get_started_example.py
@@ -3,14 +3,14 @@ from th2_data_services_lwdp.data_source import HTTPDataSource
 from th2_data_services import Data
 from datetime import datetime
 
-from th2_data_services_lwdp.streams import Streams
+from th2_data_services_lwdp.streams import Streams, Stream
 
 # [1] Create data source object to connect to lightweight data provider.
 DEMO_HOST = "10.100.66.105"  # Note that this host is only accessible from exactpro domain
 DEMO_PORT = "32681"
 data_source = HTTPDataSource(f"http://{DEMO_HOST}:{DEMO_PORT}")
 
-# [2] Getting books, scopes, groups and aliases.
+# [2] Getting books, pages, scopes, groups and aliases.
 # [2.1] Get books.
 # On database data is segregated with books, such as they never intersect.
 # To get the names of the books we have a command GetBooks which takes no argument:
@@ -20,6 +20,8 @@ books = data_source.command(commands.GetBooks())
 book_id = "demo_book_1"  # demo_book_1 is an example book from host namespace
 page_name = "1"  # example page name from book demo_book_1
 scopes = ["th2-scope"]
+# For SSE GetMessageByStream commands we have three choices for streams argument:
+# We can use a list of strings.
 streams = [
     "default-message-producer-alias",
     "fix-demo-server1",
@@ -27,19 +29,42 @@ streams = [
     "fix-client2",
     "fix-client1",
 ]
+# A list of Stream objects.
+streams_list_with_stream_object = [
+    Stream("default-message-producer-alias"),
+    Stream("fix-demo-server1"),
+    Stream("fix-demo-server2"),
+    Stream("fix-client1"),
+    Stream("fix-client2"),
+]
+# Or a Streams object, which takes a list of strings as argument to initialize.
+streams_with_streams_class = Streams(streams)
+
 groups = streams  # In this namespace groups and streams have same name.
 
-# [2.3] Get scopes.
+# Date has to be in utc timezone.
+START_TIME = datetime(year=2023, month=1, day=5, hour=13, minute=57, second=5, microsecond=0)
+END_TIME = datetime(year=2023, month=1, day=5, hour=13, minute=57, second=6, microsecond=0)
+
+# [2.3] Get pages.
+# This command returns objects of Page class
+# GetPages with only book_id will get all pages.
+pages_all: Data = data_source.command(commands.GetPages(book_id))
+
+# GetPages with timestamps will get all pages within that time frame.
+pages: Data = data_source.command(commands.GetPages(book_id, START_TIME, END_TIME))
+
+# [2.4] Get scopes.
 # In books data is partitioned even more.
 # Events are grouped by scopes, which we can get using GetScopes command:
 book_scopes = data_source.command(commands.GetEventScopes(book_id))
 
-# [2.4] Get message groups.
+# [2.5] Get message groups.
 # Messages are separated by groups and aliases.
 # To get groups we use GetMessageGroups from commands:
 book_groups = data_source.command(commands.GetMessageGroups(book_id))
 
-# [2.4] Get message aliases.
+# [2.6] Get message aliases.
 # For aliases, GetMessageAliases.
 aliases = data_source.command(commands.GetMessageAliases(book_id))
 
@@ -77,9 +102,6 @@ multiple_messages = data_source.command(
 
 # [3.3] SSE commands for events.
 # We can get events without knowing their ids beforehand, using SSE requests from the server with GetEvents command:
-# Date has to be in utc timezone.
-START_TIME = datetime(year=2023, month=1, day=5, hour=13, minute=57, second=5, microsecond=0)
-END_TIME = datetime(year=2023, month=1, day=5, hour=13, minute=57, second=6, microsecond=0)
 events: Data = data_source.command(
     commands.GetEventsByBookByScopes(
         start_timestamp=START_TIME, end_timestamp=END_TIME, book_id=book_id, scopes=scopes
@@ -107,11 +129,7 @@ messages_by_group: Data = data_source.command(
 
 # [3.5] Getting data by pages.
 # For every SSE command we have other variation, which matches data by specific page instead of datetime range.
-# GetPages with only book_id will get all pages.
-pages_all: Data = data_source.command(commands.GetPages(book_id))
 
-# GetPages with timestamps will get all pages within that time frame.
-pages: Data = data_source.command(commands.GetPages(book_id, START_TIME, END_TIME))
 page = list(pages)[0]
 
 # GetByPage commands accept Page class objects as argument.
