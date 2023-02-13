ef96302e8165bf483905701406139eab7b85353a
Slava Ermakov <connect-soft@yandex.ru>
2023-02-13 22:35:32 +0530

    Add etc integration test

---
+++ tests/tests_integration/test_etc.py
@@ -0,0 +1,17 @@
+from th2.data_services.data import Data
+from th2.data_services.event_tree import EventTreeCollection
+
+from th2.data_services.data_source.lwdp.data_source import HTTPDataSource
+from th2.data_services.data_source.lwdp.event_tree import HttpETCDriver
+
+from tests.tests_integration.conftest import DataCase
+
+
+def test_etc(all_events: DataCase, http_data_source: HTTPDataSource):
+    """Just check, that we can create ETC using our driver."""
+    events: Data[dict] = all_events.data
+    etc = EventTreeCollection(driver=HttpETCDriver(http_data_source))
+    etc.build(events)
+    etc.recover_unknown_events()
+    assert 21 == len(etc)
+    assert 1 == len(etc.get_trees())
