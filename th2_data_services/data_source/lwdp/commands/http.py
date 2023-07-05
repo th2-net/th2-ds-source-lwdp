e1de026230807981e889f87597f3c65e2465a8ee
turikotura <turikotura@outlook.com>
2023-07-05 14:11:15 +0400

    Formatting fix

--- th2_data_services/data_source/lwdp/commands/http.py
+++ th2_data_services/data_source/lwdp/commands/http.py
@@ -1306,7 +1306,6 @@ class DownloadMessagesByBookByGroupsGzip(IHTTPCommand):
         _check_list_or_tuple(self._groups, var_name="groups")
         _check_list_or_tuple(self._streams, var_name="streams")
 
-
     def handle(self, data_source: DataSource):
         api = data_source.source_api
         urls = api.get_download_messages(
