89268d9677e525c5ef0c36bee2a0a2f2f459f154
turikotura <turikotura@outlook.com>
2024-02-22 17:32:55 +0400

    Updated tests

--- tests/tests_unit/tests_common/test_utils.py
+++ tests/tests_unit/tests_common/test_utils.py
@@ -6,4 +6,4 @@ from th2_data_services.data_source.lwdp.utils import _check_datetime
 def test_datetime_invalid_type():
     with pytest.raises(Exception) as err:
         _check_datetime(datetime.now().timestamp())
-    assert "Provided timestamp should be `datetime` object" in str(err)
+    assert "Provided timestamp should be `datetime`, `str` or `int` object in UTC time" in str(err)
