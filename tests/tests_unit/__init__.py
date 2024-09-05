a4a419cecbd617b6eb34a455141670dca798964b
DavitMamrikishvili <davit.mamrikishvili1@gmail.com>
2024-09-05 15:49:11 +0400

    Update __init__.py

--- tests/tests_unit/__init__.py
+++ tests/tests_unit/__init__.py
@@ -1,5 +1,6 @@
 DEMO_PORT = "32681"  # LwDP provider v2
 
+from th2_data_services.interfaces import IEventStub, IMessageStub # noqa
 from th2_data_services.data_source.lwdp.data_source import DataSource  # noqa
 from th2_data_services.data_source.lwdp.commands import http  # noqa
 from th2_data_services.data_source.lwdp.filters.filter import Filter  # noqa
